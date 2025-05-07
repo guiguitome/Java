0ced469e4dea3d1669582f8df8b9b2d8b42afb1a
guiguitome <guitomef@gmail.com>
2025-05-07 08:48:29 -0300

    hello World 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,18 @@ package Java;
  * 
  * public -> modificador de acesso, quando está "public  significa que pode ser acessado em qualquer parte
  * do código
+ * 
+ * static -> indica que pertence à classe, não ao objeto
+ * 
+ * void -> retorno da função main, siginfica que não será retornado valor algum
+ * 
+ * String[] args -> argumentos escritos nas linhas de comando serão passadas pra esse vetor de strings
+ * 
+ * System -> biblioteca padrão do Java
+ * 
+ * out -> rodará no console
+ * 
+ * println -> print é um método pra imprime algo na tela, 'ln' ele quebra linha
  */
 public class HelloWorld {
     public static void main(String[] args) {
