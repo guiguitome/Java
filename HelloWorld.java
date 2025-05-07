d5eb2663be198fb5c8da37c5bb7eaed8615f2da8
guiguitome <guitomef@gmail.com>
2025-05-06 23:28:44 -0300

    Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package Java;
+/*
+ * explicando hello world em java:
+ * class -> é a classe, estrutura básica de um progrma java
+ * 
+ * public -> modificador de acesso, quando está "public  significa que pode ser acessado em qualquer parte
+ * do código
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
